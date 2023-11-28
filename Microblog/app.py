e6608566d8a9486afed4fee0e807508f55d46c80
Vennela <96403605+Vennelareddycs@users.noreply.github.com>
2023-11-28 18:18:45 +0530

    updated changes

--- Microblog/app.py
+++ Microblog/app.py
@@ -1,7 +1,26 @@
-from flask import Flask, render_template
+import datetime
+from flask import Flask, render_template, request
+from pymongo import MongoClient
 
 app = Flask(__name__)
+client = MongoClient("mongodb+srv://vennela24:Ve24mdb@cluster0.2pspl2c.mongodb.net/")
+app.db= client.cluster0
+entries = []
 
-@app.route("/")
+@app.route("/", methods=["GET","POST"])
 def home():
-    return render_template("home.html")
+    print([e for e in app.db.entries.find({})])
+    if request.method == "POST":
+        entry_content = request.form.get('content')
+        formatted_date=datetime.datetime.today().strftime("%Y-%m-%d")
+        entries.append((entry_content, formatted_date))
+
+    entries_with_date = [
+        (
+        entry[0],
+        entry[1],
+        datetime.datetime.strptime(entry[1], "%Y-%m-%d").strftime("%b %d")
+    )
+        for entry in entries
+    ]
+    return render_template("home.html", entries=entries_with_date)
